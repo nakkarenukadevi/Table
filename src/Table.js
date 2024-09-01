050b9dd3e742ffcb3efd4bde3abad89fd190e450
nakkarenukadevi <renukadevi9724@gmail.com>
2024-09-01 21:57:55 +0530

    add sortfunction

--- src/Table.js
+++ src/Table.js
@@ -1,54 +1,115 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Table = (props) => {
 
     let { data } = props;
-    let [fdata, setFdata] = useState(data)
 
-    const handlepersonData = (e) => {
-        console.log(e.target.innerText);
+    let [fdata, setFdata] = useState(data);
 
-        let sfilterData = data.filter((s) => {
-            if (s.name.includes(e.target.innerText)) {
-                return true;
-            } else {
-                return false;
-            }
+    let [filterConfig, setFilterConfig] = useState({});
+
+
+    let myelement = useRef(null)
+
+    const handleData = (e) => {
+        e.preventDefault()
+        setFilterConfig(prveState => ({ ...prveState, [e.target.name]: e.target.value }))
+        console.log({ ...filterConfig, [e.target.name]: e.target.value })
+
+    }
+    const handlefrom = () => {
+        let filterdata = data.filter((data) => {
+
+            let newFilterConfig = Object.entries(filterConfig);
+
+            return newFilterConfig.every(([key, value]) => {
+                return data[key] === value;
+
+            })
+
 
         });
-        setFdata(sfilterData)
+
+        setFdata(filterdata)
     }
 
+    const showfilterdiv = () => {
+        const element = myelement.current;
+
+        if (element.classList.contains('showSearch')) {
+            element.classList.toggle('active');
+        }
+
+    }
+
+    const handleSort = () => {
+        let data2 = [...fdata];
+        data2.sort((a, b) => a.name.localeCompare(b.name));
+        setFdata(data2)
+    }
     return (
-        <div>
-            <table className="border-separate border-spacing-2 border border-slate-400 mx-auto my-10">
-                <thead>
-                    <tr>
-                        <th className="border border-slate-300 p-3">Student Id</th>
-                        <th className="border border-slate-300 p-3">Student Name</th>
-                        <th className="border border-slate-300 p-3">English</th>
-                        <th className="border border-slate-300 p-3">Telugu</th>
-                        <th className="border border-slate-300 p-3">maths</th>
-                        <th className="border border-slate-300 p-3">Science</th>
-                        <th className="border border-slate-300 p-3">Social</th>
+        <div className='flex justify-center '>
+            <div onClick={showfilterdiv} className='my-10 mx-10'>    Filter</div>
+            <div class="my-10 border-2 border-slate-300 showSearch" ref={myelement} >
+
+                <table className='mt-10' >
+
+                    <tr className=' flex justify-between'>
+                        <div>Name</div>
+                        <div> <td className=''><input type="text" value={fdata.name} className=' border-2 border-slate-400' name='name' onChange={(e) => { handleData(e) }} /></td></div>
                     </tr>
-                </thead>
-                <tbody>
-                    {fdata.map((sdata) => {
-                        return <tr key={sdata.id} >
-                            <td className="border border-slate-300 p-2 text-center">{sdata.id}</td>
-                            <td className="border border-slate-300 p-2 " onClick={handlepersonData}>{sdata.name}  </td>
-                            <td className="border border-slate-300 p-2 text-center">{sdata.english}</td>
-                            <td className="border border-slate-300 p-2 text-center">{sdata.telugu}</td>
-                            <td className="border border-slate-300 p-2 text-center">{sdata.maths}</td>
-                            <td className="border border-slate-300 p-2 text-center">{sdata.Science}</td>
-                            <td className="border border-slate-300 p-2 text-center">{sdata.social}</td>
+                    <tr className=' flex justify-between'>
+                        <div> City</div>
+                        <div> <td className=''><input type="text" value={fdata.city} className=' border-2 border-slate-400' name="city" onChange={(e) => { handleData(e) }} /></td></div>
+                    </tr>
+                    <tr className=' flex justify-between'>
+                        <div>Work</div>
+                        <div> <td className=''><input type="text" value={fdata.work} className=' border-2 border-slate-400' name="work" onChange={(e) => { handleData(e) }} /></td></div>
+                    </tr>
+                    <tr className=' flex justify-between'>
+                        <div> Salary</div>
+                        <div> <td className=''><input type="number" value={fdata.salary} className=' border-2 border-slate-400' name="salary" onChange={(e) => { handleData(e) }} /></td></div>
+                    </tr>
+                    <tr className=' flex justify-between'>
+                        <div> address</div>
+                        <div><td className=''><input type="text" value={fdata.address} className=' border-2 border-slate-400' name="address" onChange={(e) => { handleData(e) }} /></td></div>
+                    </tr>
+                    <button onClick={handlefrom} className='my-3 bg-blue-600 text-white border-2  p-3'>clickMe</button>
+                </table>
+
+
+
+            </div>
+            <div>
+                <table className="border border-slate-300  my-10">
+                    <thead>
+                        <tr className="border-b border-gray-300" >
+                            <th className=" p-4">Id</th>
+                            <th className=" p-4" onClick={handleSort}>Name</th>
+                            <th className=" p-4">City</th>
+                            <th className=" p-4">work</th>
+                            <th className=" p-4">salary</th>
+                            <th className=" p-4">address</th>
 
                         </tr>
-                    })}
+                    </thead>
+                    <tbody>
+                        {fdata.map((sdata) => {
+                            return <tr key={sdata.id} className="border-b border-gray-300" >
+                                <td className=" p-4 text-center">{sdata.id}</td>
+                                <td className=" p-4 ">{sdata.name}  </td>
+                                <td className=" p-4 text-center">{sdata.city}</td>
+                                <td className=" p-4 text-center">{sdata.work}</td>
+                                <td className=" p-4 text-center">{sdata.salary}</td>
+                                <td className=" p-4 text-center">{sdata.address}</td>
 
-                </tbody>
-            </table>
+
+                            </tr>
+                        })}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
--- src/utilis/utilis.js
+++ src/utilis/utilis.js
@@ -2,91 +2,90 @@ export const studentData = [
     {
         id: 1,
         name: "bhanu",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "guntur",
+        work: "software",
+        address: "saradha colony",
+        salary: 200000
+
     },
     {
         id: 2,
-        name: "bhanuPraksh",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        name: "bhanuPrakash",
+        city: "vijayawada",
+        work: "devloper",
+        address: "brodipet",
+        salary: 80000
     },
     {
         id: 3,
         name: "bhanupriya",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "kadapa",
+        work: "software",
+        address: "gandhi nagar",
+        salary: 70000
     },
     {
         id: 4,
         name: "Rohit",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "guntur",
+        work: "doctor",
+        address: "saradha colony",
+        salary: 200000
     },
     {
         id: 5,
         name: "venkatrohith",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "retur",
+        work: "doctor",
+        address: "near sivalayam",
+        salary: 100000
     },
     {
         id: 6,
         name: "jyothish",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "retur",
+        work: "backand devloper",
+        address: "saradha colony",
+        salary: 200000
     },
     {
         id: 7,
         name: "renuka",
-        english: 98,
-        maths: 100,
-        telugu: 99,
-        social: 97,
-        Science: 100
+        city: "guntur",
+        work: "software",
+        address: "saradha colony",
+        salary: 20000
     },
     {
         id: 8,
         name: "renuka devi",
-        english: 70,
-        maths: 80,
-        telugu: 89,
-        social: 97,
-        Science: 78
+        city: "guntur",
+        work: "front end devloper",
+        address: "saradha colony",
+        salary: 50000
     },
     {
         id: 9,
         name: "archana",
-        english: 70,
-        maths: 80,
-        telugu: 99,
-        social: 97,
-        Science: 80
+        city: "jellamudi",
+        work: "bank",
+        address: "saradha colony",
+        salary: 60000
     },
     {
         id: 10,
         name: "praveen",
-        english: 88,
-        maths: 90,
-        telugu: 95,
-        social: 96,
-        Science: 87
+        city: "jellamudi",
+        work: "software",
+        address: "saradha colony",
+        salary: 90000
+    },
+    {
+        id: 11,
+        name: "bhanu sai",
+        city: "guntur",
+        work: "software",
+        address: "saradha colony",
+        salary: 9000
     }
 ]
